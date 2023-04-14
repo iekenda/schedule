f8542cc258e78dd9b8b8dc9ac1689fa5997b905c
家村健太郎 <aho@iemurakentarounoMacBook-Air.local>
2023-04-14 10:29:14 +0900

    バリデーション修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ApplicationRecord
    validates :ending,  presence: true
    validates :memo, length: { maximum: 500 }
    validate :start_end_check
+   validate :date_before_start
 
   def start_end_check
     starting ||= 1
@@ -13,4 +14,9 @@ class Task < ApplicationRecord
     errors.add(:ending, "は開始日より前の日付は登録できません。") unless
     starting < ending 
     end
+  end
+
+  def date_before_start
+    return if starting.blank?
+    errors.add(:starting, "は今日以降のものを選択してください") if starting < Date.today
   end
